ff2e4cf254696d6c62688e2516c91c219654143e
dramos <diego.ramos@imagemaker.com>
2020-01-29 18:08:16 -0300

    First commit

---
+++ src/app/directiva/directiva.component.ts
@@ -0,0 +1,17 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html',
+})
+export class DirectivaComponent {
+  listaCurso: string[] = ['TypeScript', 'JavaScript', 'Java SE', 'C#', 'PHP'];
+  habilitar = true;
+
+  constructor() { }
+
+  setHabilitar() {
+    this.habilitar = !this.habilitar;
+  }
+
+}
